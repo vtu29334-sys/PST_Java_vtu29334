841bd1e3b3b8d372ab3d97631b6178176d50f794
vtu29334-sys <vtu29334@veltech.edu.in>
2026-07-28 19:00:33 +0530

    Create task10.java

---
+++ PST/task10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class task10 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int d1=n1%10;
+        int d2=n2%10;
+        int sum=d1+d2;
+        System.out.println(sum);
+        
+
+    }
+    
+}
